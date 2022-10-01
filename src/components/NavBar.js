1cc8437dda7b9c4f107e9d26f5b1d760de21080a
benkaidahicham <benkaida17dz@gmail.com>
2022-10-01 17:12:58 +0100

    changing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React,{ useCallback, useState} from "react";
-import { Col, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import React,{ useState} from "react";
+import { Col, Container,  Nav, Navbar } from "react-bootstrap";
 
 
 const NavBar =({filterbysearch})=> { 
